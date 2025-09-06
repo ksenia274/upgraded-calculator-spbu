9bb97037854aef17e10c8efe302ac9799a698dae
Ivan Barabanov <st128433@student.spbu.ru>
2025-09-06 17:50:03 +0300

    interface and keyboard input (#14)
    * added interface and keyboard input
    
    * added error handling and scrolling-able screen
    
    ** bugs fixed
    
    ---------
    
    Co-authored-by: Nether <4wows4nether@gmail.com>

--- composeApp/src/wasmJsMain/kotlin/spbu/wecalc/project/App.kt
+++ composeApp/src/wasmJsMain/kotlin/spbu/wecalc/project/App.kt
@@ -3,13 +3,14 @@ package spbu.wecalc.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,27 +22,11 @@ import upgraded_calculator_spbu.composeapp.generated.resources.compose_multiplat
 @Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color(0xFFB0B0B0)),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            CalculatorScreen()
         }
     }
 }
