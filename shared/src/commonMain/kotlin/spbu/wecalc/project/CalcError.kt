9746a369d6899a699dd3e6800866e7632235da01
ruavee <me@ruavee.ru>
2025-09-06 18:43:58 +0300

    fixed split exceptions

--- shared/src/commonMain/kotlin/spbu/wecalc/project/CalcError.kt
+++ shared/src/commonMain/kotlin/spbu/wecalc/project/CalcError.kt
@@ -3,4 +3,5 @@ package spbu.wecalc.project
 sealed class CalcError {
     object DivisionByZero : CalcError()
     object SyntaxError : CalcError()
+    data class InternalError(val message: String) : CalcError()
 }
--- shared/src/jvmMain/kotlin/spbu/wecalc/project/ExpressionEvaluator.kt
+++ shared/src/jvmMain/kotlin/spbu/wecalc/project/ExpressionEvaluator.kt
@@ -4,15 +4,23 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 class ExpressionEvaluator : Calculator {
     override fun evaluate(expression: String): CalcResult {
+        if (expression.isBlank()) {
+            return CalcResult.Failure(CalcError.SyntaxError)
+        }
+
         return try {
             val result = ExpressionBuilder(expression).build().evaluate()
             if (result.isInfinite() || result.isNaN()) {
                 CalcResult.Failure(CalcError.DivisionByZero)
-            } else CalcResult.Success(result)
+            } else {
+                CalcResult.Success(result)
+            }
         } catch (_: ArithmeticException) {
             CalcResult.Failure(CalcError.DivisionByZero)
-        } catch (_: Exception) {
+        } catch (_: IllegalArgumentException) {
             CalcResult.Failure(CalcError.SyntaxError)
+        } catch (_: Exception) {
+            CalcResult.Failure(CalcError.InternalError("Unknown internal error"))
         }
     }
 }
